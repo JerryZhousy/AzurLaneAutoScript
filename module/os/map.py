0f74b897cc25a49f006e7610ca4bf9bfde94a8f0
JerryZhousy <jerryzhousy@gmail.com>
2026-01-12 21:01:10 +0100

    Fix: remove duplicated functions

--- module/os/map.py
+++ module/os/map.py
@@ -940,46 +940,6 @@ class OSMap(OSFleet, Map, GlobeCamera, StorageHandler, StrategicSearchHandler):
                 in_map_timer.clear()
                 continue
 
-    def os_auto_search_run(self, drop=None, strategic=False, interrupt=None):
-        """
-        Args:
-            drop (DropRecord):
-            strategic (bool): True to use strategic search
-            interrupt (callable):
-        Returns:
-            int: Number of finished combat
-        """
-        finished_combat = 0
-        for _ in range(5):
-            backup = self.config.temporary(Campaign_UseAutoSearch=True)
-            try:
-                if strategic:
-                    self.strategic_search_start(skip_first_screenshot=True)
-                combat = self.os_auto_search_daemon(drop=drop, strategic=strategic, interrupt=interrupt)
-                finished_combat += combat
-            except CampaignEnd:
-                logger.info('OS auto search finished')
-            finally:
-                backup.recover()
-
-            # Continue if was Auto search interrupted by ash popup
-            # Break if zone cleared
-            if self.config.is_task_enabled('OpsiAshBeacon'):
-                if self.handle_ash_beacon_attack() or self.ash_popup_canceled:
-                    strategic = False
-                    continue
-                else:
-                    break
-            else:
-                if self.info_bar_count() >= 2:
-                    break
-                elif self.ash_popup_canceled:
-                    continue
-                else:
-                    break
-
-        return finished_combat
-
     @property
     def _is_siren_research_enabled(self):
         """
@@ -1109,7 +1069,7 @@ class OSMap(OSFleet, Map, GlobeCamera, StorageHandler, StrategicSearchHandler):
                        'this might be 2 adjacent fleet mechanism, stopped')
         return False
 
-    def fleets_clear_question(self, drop=None):
+    def fleets_clear_question(self, goto_main=True, skip_first_screenshot=True):
         """
         Args:
             goto_main (bool): If go to the page_main
@@ -1243,25 +1203,6 @@ class OSMap(OSFleet, Map, GlobeCamera, StorageHandler, StrategicSearchHandler):
                     break
 
         return finished_combat
-
-    def clear_question(self, drop=None):
-        """
-        Clear nearly (and 3 grids from above) question marks on radar.
-        Try 3 times at max to avoid loop tries on 2 adjacent fleet mechanism.
-
-        Args:
-            drop:
-
-        Returns:
-            bool: If cleared
-        """
-        logger.hr('Fleets Clear Question', level=2)
-        fleets = [fleet for fleet in range(1, 5) if fleet != self.config.OpsiFleet_Fleet]
-        for fleet in fleets:
-            self.fleet_set(fleet)
-            if self.clear_question(drop=drop):
-                return True
-        return False
 
     def run_auto_search(self, question=True, rescan=None, after_auto_search=True, interrupt=None):
         """
